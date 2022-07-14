727c2a1dc471a564f21f3aac37dcf9117552bb8b
Louis <louis-kaan2002@web.de>
2022-07-14 15:58:24 +0200

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls, Sky, softShadows } from "@react-three/drei";
+import { Suspense, useEffect, useRef } from "react";
+import { Canvas } from "react-three-fiber";
 
+import {
+  ACESFilmicToneMapping,
+  DoubleSide,
+  PCFSoftShadowMap,
+  PlaneGeometry,
+  sRGBEncoding,
+} from "three";
+
+softShadows();
 function App() {
+  let plane = new PlaneGeometry(100, 100, 50, 50);
+  const ground = useRef<PlaneGeometry>(plane);
+
+  const updateBufferGeometry = () => {
+    if (ground != null) {
+      let position = ground.current.getAttribute("position");
+
+      let arr = position.array;
+
+      for (let index = 0; index < arr.length; index++) {
+        console.log(arr[index]);
+      }
+
+      console.log(arr);
+
+      position.needsUpdate = true;
+    }
+  };
+
+  useEffect(() => {
+    updateBufferGeometry();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Canvas
+        shadows
+        camera={{ fov: 75, near: 1, far: 500, position: [80, 20, 80] }}
+        onCreated={({ gl }) => {
+          gl.setPixelRatio(window.devicePixelRatio);
+          gl.outputEncoding = sRGBEncoding;
+          gl.physicallyCorrectLights = true;
+          gl.shadowMap.enabled = true;
+          gl.shadowMap.type = PCFSoftShadowMap;
+          gl.toneMapping = ACESFilmicToneMapping;
+        }}
+      >
+        <Sky />
+        <OrbitControls />
+        <ambientLight intensity={1.5} castShadow color="#ffffff" />
+        <Suspense fallback={null}>
+          <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry {...plane} attach="geometry" ref={ground} />
+            <meshStandardMaterial
+              side={DoubleSide}
+              color="#7ec850"
+              attach="material"
+            />
+          </mesh>
+          <gridHelper args={[100, 50, "#000", "#000"]} />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
