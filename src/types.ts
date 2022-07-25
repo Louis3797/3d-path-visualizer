ae6276c293458a50a34fdf0fe8e9855fbe30f1e1
Louis <louis-kaan2002@web.de>
2022-07-25 15:25:53 +0200

    added Node class

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,17 @@
+import { Node } from "./utils/Node";
+
 export type Animations = {
   [name: string]: {
     clip: THREE.AnimationAction;
   };
+};
+
+export type NodeProps = {
+  x: number;
+  z: number;
+  isStart?: boolean;
+  isFinish?: boolean;
+  isWall?: boolean;
+  distance?: number;
+  prevNode?: Node | null;
 };
