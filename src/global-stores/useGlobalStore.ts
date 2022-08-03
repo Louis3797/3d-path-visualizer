07abe54979515745f14240461dff2d2cb68951d6
Louis <louis-kaan2002@web.de>
2022-08-03 15:38:55 +0200

    moved initalization off grid in store

--- src/global-stores/useGlobalStore.ts
+++ src/global-stores/useGlobalStore.ts
@@ -1,11 +1,12 @@
 import create from "zustand";
 import { GlobalState } from "../types";
+import { initializeGrid } from "../utils/InitalizeGrid";
 import { Node } from "../utils/Node";
 
 export const useGlobalStore = create<GlobalState>()((set, _get) => ({
   algorithm: "",
   speed: "",
-  grid: [],
+  grid: initializeGrid(31),
   isDragging: false,
   setAlgorithm: (newAlgorithm: string) => set({ algorithm: newAlgorithm }),
   setSpeed: (newSpeed: string) => set({ speed: newSpeed }),
