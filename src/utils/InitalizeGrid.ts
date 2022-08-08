be59434cfe83712bf7e27bd122ea21fb81b915fc
Louis <louis-kaan2002@web.de>
2022-08-08 12:52:03 +0200

    added instance Id to nodes

--- src/utils/InitalizeGrid.ts
+++ src/utils/InitalizeGrid.ts
@@ -2,10 +2,15 @@ import { Node } from "./Node";
 
 export const initializeGrid = (size: number): Node[][] => {
   const tempGraph: Node[][] = [];
+
+  // Amount of vertices in grid
+  // Amount - 1, because we start count by 0
+  let verticeAmount = Math.pow(size, 2) - 1;
+
   for (let i = 0; i < size; i++) {
     tempGraph.push([]);
     for (let j = 0; j < size; j++) {
-      tempGraph[i].push(new Node({ x: i, z: j }));
+      tempGraph[i].push(new Node({ x: i, z: j, instanceId: verticeAmount-- }));
     }
   }
 
